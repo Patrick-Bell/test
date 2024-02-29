45947a2d1dec0340e3616071519e35a45fe54ba4
Patrick <patrickbell1302@gmail.com>
2024-02-29 11:24:43 +0000

    update

--- orders.js
+++ orders.js
@@ -20,4 +20,10 @@ async function getOrdersFromTable() {
   }
 }
 
-module.exports = { addOrderToTable, getOrdersFromTable };
+function clearCart() {
+  cart = [];
+  saveToLocalStorage();
+  updateCartIcon();
+}
+
+module.exports = { addOrderToTable, getOrdersFromTable, clearCart };
