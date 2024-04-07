df4d0114390fa4f0c92e508f6ec1a5b27b761192
Patrick <patrickbell1302@gmail.com>
2024-04-07 23:41:20 +0100

    update

--- server.js
+++ server.js
@@ -521,8 +521,8 @@ app.post("/stripe-checkout", async (req, res) => {
           },
           allow_promotion_codes: true,
         mode: "payment",
-        success_url: "/success?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: "http://localhost:3000/cancel.html?session_id={CHECKOUT_SESSION_ID}",
+        success_url: "https://test-admin-wdmf.onrender.com/success.html?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "https://test-admin-wdmf.onrender.com/cancel.html?session_id={CHECKOUT_SESSION_ID}",
         billing_address_collection: "required",
         line_items: lineItems, 
     })
